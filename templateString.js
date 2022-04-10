d61b56d1b80926e010bfd22a38e4f32e54016c7c
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-04-10 18:34:19 +0300

    add template string example

---
+++ templateString.js
@@ -0,0 +1,15 @@
+/**
+ * template String : 
+ * يجب وضع النص ضمن  اشارة تنصيص مفردة و مائلة `
+ * يمكن كتابة اسطر متعددة  
+ * يمكن وضع متغير ضمن نص داخل تعبير  ${varName} ويمكن وضع تعبير مثل تعبير شرطي داخله 
+ * 
+ */
+let name = 'ali' ;
+let age     = 21 ;
+
+let str = `
+Me Name is ${name} 
+Me Age is  ${age}
+`;
+console.log(str);
