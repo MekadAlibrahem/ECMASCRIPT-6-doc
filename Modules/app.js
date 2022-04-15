afc5a0721687b37e2db808141797f8e968dbd909
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-04-15 17:06:07 +0300

    add module Part 1 Export & Import Example

---
+++ Modules/app.js
@@ -0,0 +1,16 @@
+// Important Note  : Run Live Server Before run code 
+const a =1 ;
+ let myArray = [1,2,3,4,5];
+ function sayHello(name){
+    return `Hello ${name}` ;
+}
+
+export {
+    a , 
+    myArray ,
+    sayHello
+}
+/**
+ * can use export befor variable or method ...  name or in export { all variable and method Export  }
+ */
+
---
+++ Modules/main.js
@@ -0,0 +1,10 @@
+/**
+ * Modules Part 1  Export & import 
+ *   
+ */
+
+import { a , myArray , sayHello} from './app.js' ;
+console.log(a);
+console.log(myArray);
+console.log(sayHello("ali"));
+
