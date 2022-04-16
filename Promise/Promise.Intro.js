21bb28e8778bb50dc58ec2c52274c12f21c50f06
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-04-16 14:45:10 +0300

    add Promise Intro

---
+++ Promise/Promise.Intro.js
@@ -0,0 +1,26 @@
+/**
+ * Promise Intro 
+ * ---- Promise Make It Easy To Support Asynchronous Programming 
+ * ---- Promise In JavaScript Is Like Promise In Real Life  
+ * ---- Promise Is Something That Will Happen in The Future 
+ * ---- Promise Is A Placeholder For A Future Value 
+ * Example : Your Ordered Food From Restaurant  
+ * ---- [ Promise ] Is The Order Record 
+ * ---- [ The Future Value ] Is Food You Will Receive 
+ * 
+ * Promise Is An Object 
+ * Promise Is The Event Completion Or Failure of Async Operation $ Its Resulting Value 
+ * JavaScript Use Callbacks For Asynchronous Programming 
+ * - [1] Function Called And Do The Task 
+ * - [2] Action Complete 
+ * - [3] Another Function Called 
+ * - [4] Action Complete 
+ * - [5] Another Function Called 
+ * - In Complex Cases  => Every Call Add Nasting Level
+ * more information : http://callbackhell.com/
+ * Promise Avoiding Callback Hell Or Pyramid Of Doom
+ * Promise Is One Of This States 
+ *  [1] Pending     => Not Fulfilled Or Rejected 
+ *  [2] Fulfilled   => Operation succeeded 
+ *  [3] Rejected    => Operation Failed
+ */
