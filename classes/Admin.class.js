653b15958c95239aa4feeaebac00ed5f0d677a54
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-04-16 12:35:41 +0300

    add classes part 2  Import & Export Classes  Example

---
+++ classes/Admin.class.js
@@ -0,0 +1,12 @@
+
+import User from "./User.class.js";
+
+
+class Admin extends User{
+    constructor(name , email){
+        super(name  , email);
+    }
+}
+
+
+export default Admin ;
---
+++ classes/part2.js
@@ -0,0 +1,11 @@
+
+/**
+ * classes  Part 2  
+ * export and Import classes  
+ */
+import User from "./User.class.js";
+import Admin from "./Admin.class.js";
+
+let Admin1 = new Admin("Osama" ,  "o@nn.sa");
+Admin1.getInfo();
+
